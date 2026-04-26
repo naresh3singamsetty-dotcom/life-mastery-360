7f34907560390d6f5613f52346dd2e321b97cd45
Naresh Singamsetty <naresh3.singamsetty@gmail.com>
2026-04-26 14:18:16 -0400

    Fix React render error - display option.label instead of object in guest assessment buttons

--- frontend/src/pages/GuestAssessmentPage.jsx
+++ frontend/src/pages/GuestAssessmentPage.jsx
@@ -255,7 +255,7 @@ export default function GuestAssessmentPage() {
                     }
                   }}
                 >
-                  {option}
+                  {option.label}
                 </button>
               ))}
             </div>
